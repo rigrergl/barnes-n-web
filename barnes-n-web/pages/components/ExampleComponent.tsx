ef4ac8eef916bff36b0d5dc8896873cae47b6350
rigrergl <rigrergl@gmail>
2022-10-05 13:14:19 -0500

    imported bootstrap and modified example component to use it

--- barnes-n-web/pages/components/ExampleComponent.tsx
+++ barnes-n-web/pages/components/ExampleComponent.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState, ReactElement } from 'react'
+import Toast from 'react-bootstrap/Toast';
+import Button from 'react-bootstrap/Button'
 
-function ExampleComponent() {
-  return (
-    <div>ExampleComponent</div>
-  )
+type Props = {
+    children: ReactElement
+}
+
+function ExampleComponent(props: Props) {
+    const [showToast, toggleShowToast] = useState(true);
+
+    return (
+        <>
+            {!showToast && <Button onClick={() => toggleShowToast(true)}>Show Toast</Button>}
+            <Toast show={showToast} onClose={() => toggleShowToast(false)}>
+                <Toast.Header>
+                    <strong className="mr-auto">React-Bootstrap</strong>
+                </Toast.Header>
+                <Toast.Body>{props.children}</Toast.Body>
+            </Toast>
+        </>
+    )
 }
 
 export default ExampleComponent
