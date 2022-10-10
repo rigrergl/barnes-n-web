d39ed134c0224dcb2b1de92b0c790ebc2f10ca30
rigrergl <rigrergl@gmail>
2022-10-10 07:04:21 -0500

    db setup working

---
+++ barnes-n-web/pages/api/test/users/index.ts
@@ -0,0 +1,48 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import connectionPool from '../../../../lib/db';
+import secureLog from '../../../../lib/SecureLog';
+import SecureLog from '../../../../lib/SecureLog';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const httpMethod = req.method;
+
+    switch (httpMethod) {
+        case 'GET':
+            await GET(req, res);
+            break;
+        case 'POST':
+            await POST(req, res);
+            break;
+        case 'DELETE':
+            await DELETE(req, res);
+            break;
+        default:
+            res.setHeader('Allow', ['GET', 'POST', 'DELETE']);
+            res.status(405).end(`Method ${httpMethod} Not Allowed`);
+
+    }
+}
+
+async function GET(req: NextApiRequest, res: NextApiResponse) {
+    const query = "SELECT * FROM TEST_USER"
+
+    try {
+        connectionPool.query(query, function (error: any, results: any, fields: any) {
+            if (error) throw error;
+            results = results.map((v: any) => Object.assign({}, v));
+            res.status(200).json(results);
+        });
+    } catch (error) {
+        secureLog(error);
+    }
+}
+
+async function POST(req: NextApiRequest, res: NextApiResponse) {
+    //TODO
+    res.status(200);
+}
+
+async function DELETE(req: NextApiRequest, res: NextApiResponse) {
+    //TODO
+    res.status(200);
+}
