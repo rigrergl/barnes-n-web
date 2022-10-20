f7b81566ad30e9ca4c22cad6d8a41f50f7b8cd3e
BrandonT80 <31973893+BrandonT80@users.noreply.github.com>
2022-10-20 18:03:38 -0500

    Header Completed
    Completed Header Component, Started Login Component to show header

--- barnes-n-web/pages/components/Header.js
+++ barnes-n-web/pages/components/Header.js
@@ -4,16 +4,14 @@ const Header = () => {
   return (
     <div className='header'>
       <div className='headerRow'>
-        <div className='headerColumn'>
+        <div className='headerColumn8'>
           <div style={{padding: 12.5}}>
             <img src='./images/logo.png' style={{width: 75, height: 75, float:'left'}} />
           </div>
         </div>
-        <div className='headerColumn'>
-          <p>&nbsp;</p>
-          <div className='fontSansitaOne'>
-            <p>Barnes-N-Web</p>
-          </div>
+        <div className='headerColumn15'>
+        <p>&nbsp;</p>
+            <p style={{fontSize: 30}}>Barnes-N-Web</p>
         </div>
       </div>
     </div>
