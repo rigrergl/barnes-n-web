dcb4c75602b2aa2c4064d53ee0dd95406f079af2
Tisbet <tisbetanaya@gmail.com>
2022-10-21 12:34:36 -0500

    CSS changes

--- barnes-n-web/pages/Search.tsx
+++ barnes-n-web/pages/Search.tsx
@@ -8,43 +8,51 @@ const Search = () => {
         <div className="row">
           {/* Search Section */}
           <div className="column">
-            <label style={{ textAlign: "center", fontSize: "25px" }}>
-              Search
-            </label>
+            <label className="searchText">Search</label>
             <form>
               <label>
                 <p>Location</p>
-                <input type="text" placeholder="City"></input>
-                <input type="text" placeholder="Zip Code"></input>
+                <input
+                  className="searchInputBox"
+                  type="text"
+                  placeholder="City"
+                />
+                <input
+                  className="searchInputBox"
+                  type="text"
+                  placeholder="Zip Code"
+                />
               </label>
               <label>
                 <p>Search Criteria</p>
-                <input type="text" placeholder="Title"></input>
-                <input type="text" placeholder="ISBN 13"></input>
-                <input type="text" placeholder="ISBN 10"></input>
-                <input type="text" placeholder="Author"></input>
+                <input
+                  className="searchInputBox"
+                  type="text"
+                  placeholder="Title"
+                />
+                <input
+                  className="searchInputBox"
+                  type="text"
+                  placeholder="ISBN 13"
+                />
+                <input
+                  className="searchInputBox"
+                  type="text"
+                  placeholder="ISBN 10"
+                />
+                <input
+                  className="searchInputBox"
+                  type="text"
+                  placeholder="Author"
+                />
               </label>
               {/* Submit Button */}
-              <input
-                style={{
-                  textAlign: "center",
-                  top: "150px",
-                  fontSize: "15px",
-                  height: "40px",
-                  width: "150px",
-                  background: "#FB8500",
-                  borderRadius: "25px",
-                }}
-                type="submit"
-                value="Search"
-              />
+              <input className="searchButton" type="submit" value="Search" />
             </form>
           </div>
           {/* Results Section */}
           <div className="column">
-            <label style={{ textAlign: "center", fontSize: "25px" }}>
-              Results
-            </label>
+            <label className="searchText">Results</label>
             <div className="resultsBox"></div>
           </div>
         </div>
