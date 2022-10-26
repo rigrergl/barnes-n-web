65a4871c5dc8c6d23e05f2da040d4e6c66406de3
rigrergl <rigrergl@gmail>
2022-10-25 22:24:23 -0500

    turned verifyToken synchronous, more left to do

--- barnes-n-web/lib/verifyToken.ts
+++ barnes-n-web/lib/verifyToken.ts
@@ -1,12 +1,16 @@
 const jwt = require('jsonwebtoken');
+import secureLog from '@/lib/SecureLog';
 
 const publicKeyBase64: String = process.env.SERVER_PUBLIC_KEY || "";
 const publicKey = Buffer.from(publicKeyBase64, 'base64').toString('utf8');
 
 var verifyToken = (token: any) => {
-    jwt.verify(token, publicKey, (err: any, decoded: any) => {
-        // TODO: do something 
-    });
+    try {
+        const decoded = jwt.verify(token, publicKey);
+        console.log(decoded);
+    } catch (err) {
+        secureLog(err);
+    }
 }
 
 export default verifyToken;
