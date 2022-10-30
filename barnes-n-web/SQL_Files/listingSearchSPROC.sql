10159b1feee6bd7f130a37cfab1f70d5441d614d
rigrergl <rigrergl@gmail>
2022-10-29 23:24:48 -0500

    removed debug info from SPROC and renamed folder

---
+++ barnes-n-web/SQL_Files/listingSearchSPROC.sql
@@ -0,0 +1,21 @@
+DROP PROCEDURE IF EXISTS SearchListings;
+
+DELIMITER //
+
+CREATE PROCEDURE SearchListings(
+	IN bookTitle VARCHAR(255),
+    IN locationInfo VARCHAR(30), -- in the format 'POINT(x.xxx x.xxx)'
+    IN maxDistanceInMeters INT
+)
+BEGIN
+	SET @userLocation = ST_GeomFromText(locationInfo, 4326);
+
+	DROP TEMPORARY TABLE IF EXISTS locationsInRange;
+	CREATE TEMPORARY TABLE locationsInRange AS
+		SELECT location_id, ST_Distance_Sphere(position, @userLocation) as distance_m FROM GeoLocation HAVING distance_m <= maxDistanceInMeters;
+
+	SELECT * FROM Listings l
+    WHERE l.title = bookTitle AND l.location_id IN (SELECT location_id FROM locationsInRange);
+END //
+
+DELIMITER ;
